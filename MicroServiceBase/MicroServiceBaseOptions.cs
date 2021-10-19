59a524ad8fcb958a7d55e2ad0042c639accef383
dmdevcode <91370068+dmdevcode@users.noreply.github.com>
2021-10-19 20:21:26 +0200

    Add project files.

---
+++ MicroServiceBase/MicroServiceBaseOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroServiceBase
+{
+    public class MicroServiceBaseOptions
+    {
+        // add your own settings here
+        public string LogDirectory { get; set; }
+        public string DataDirectory { get; set; }
+    }
+}
